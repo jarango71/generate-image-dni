4023700bb6fee539612aaf426d5a3a3052d8a348
jarango71 <jarango71@gmail.com>
2023-10-05 14:24:39 -0500

    Update Version 1.0.3

--- src/main/java/rc/dni/GenerateImage.java
+++ src/main/java/rc/dni/GenerateImage.java
@@ -99,18 +99,16 @@ public class GenerateImage {
 		        int compositeRule = AlphaComposite.SRC_OVER;
 		        AlphaComposite ac;
 
-                byte[] pixels = Base64.getDecoder().decode(person.getFotoPersona());
-                
-                BufferedImage foto = new BufferedImage(ANCHO, ALTO, BufferedImage.TYPE_BYTE_GRAY);
-                foto.getRaster().setDataElements(0, 0, ANCHO, ALTO, pixels);
-                
-		        /*if(foto.getWidth() > ANCHO) {
+                byte[] decodedBytes = Base64.getDecoder().decode(person.getFotoPersona());
+                InputStream inputStream = new ByteArrayInputStream(decodedBytes);
+		        BufferedImage foto = ImageIO.read(inputStream);
+		        if(foto.getWidth() > ANCHO) {
 		        	BufferedImage resizedImage = new BufferedImage(ANCHO, ALTO, BufferedImage.TYPE_BYTE_GRAY);
 		        	Graphics2D gFotoAux = resizedImage.createGraphics();
 		        	gFotoAux.drawImage(foto, 0, 0, ANCHO, ALTO, null);
 		        	gFotoAux.dispose();
 		        	foto = resizedImage;
-		        }*/
+		        }
 		        ac = AlphaComposite.getInstance(compositeRule, alpha);
 		        w.drawImage(procesada,0,0,null);
 		        w.setComposite(ac);
